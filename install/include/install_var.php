7d1fddee7f7b05a631f38a1a6a07bfdad7aa813b
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-10-24 17:20:40 +0800

    update

--- install/include/install_var.php
+++ install/include/install_var.php
@@ -61,6 +61,7 @@ define('ENV_CHECK_ERROR', 31);
 define('UNDEFINE_FUNC', 32);
 define('MISSING_PARAMETER', 33);
 define('LOCK_FILE_NOT_TOUCH', 34);
+define('TIMESTAMP', time());
 
 if(function_exists('mysqli_connect')) $func_items = array('mysqli_connect',  'xml_parser_create', 'curl_init');
 else $func_items = array('mysql_connect',  'xml_parser_create', 'curl_init');
